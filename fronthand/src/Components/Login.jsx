3264a73eeffef01e0d67c0afde3698eb2529945f
yuvalprogramer@gmail.com <yuvalprogramer@gmail.com>
2023-10-28 10:28:50 +0300

    adding gifimg and changing files

--- fronthand/src/Components/Login.jsx
+++ fronthand/src/Components/Login.jsx
@@ -1,11 +1,20 @@
 
 import '../style/LoginStyle.css';
-
+import { Grid } from '@mui/material';
+import logo from '../giphy.gif';
 function RenderLogin(){
 
     return(
     <div className="LoginScreen">
-       
+        <Grid container style={{minHeight:'100vh'}}>
+            <Grid item xs={12} sm={6}>
+                <img src = {logo}
+                     style={{width:'100%', height:'100%', objectFit:'cover'}}alt='brand'></img>
+            </Grid>
+            <Grid container item xs={12} sm={6} style={{padding:10}}>
+                
+            </Grid>
+        </Grid>
     </div>
     );
 }
